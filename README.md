a1c97d99cef3ba137c4a0cc163485cfb02633788
Nathan M <nz.nathan@gmail.com>
2018-06-30 11:24:42 +1200

    0.2.0 release

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 **LinkIt** is a simple jQuery plugin to attach links to elements
 
-##Version
-0.1.0
+##Version##
+0.2.0
 
 ##Usages
-
+     $('span').linkIt({
+                   href: 'http://google.com',
+                   text: 'Click Me',
+                   target: '_blank'
+     });
 
 ##Vendors
 jQuery - [http://query.com](http://jqery.com)
---
+++ jquery.linkIt.js
@@ -0,0 +1,36 @@
+/*
+*name: LinkIt
+* author: Nathan M
+* version: 0.1.0
+* license: MIT
+ */
+
+(function($){
+    $.fn.linkIt = function (options) {
+        //Default settings
+        var settings = $.extend({
+            href : null,
+            text: null,
+            target: '_self'
+        }, options);
+
+        //Valadation
+        if (settings.href == null){
+            console.log('You need an href option for LinkIt to work');
+            return this;
+        }
+
+        //Return
+        return this.each(function () {
+           var object = $(this);
+
+           if (settings.text == null){
+               settings.text = object.text();
+           }
+
+            object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+        });
+
+
+    }
+}(jQuery));
